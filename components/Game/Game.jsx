2fa72837e97c62320a490e4adb6745dcc8d9f6b6
Caelan Williams <xsomedaykk@gmail.com>
2022-01-14 01:06:24 -0800

    update layout

--- components/Game/Game.jsx
+++ components/Game/Game.jsx
@@ -4,7 +4,8 @@ export default function Game() {
     return (
         <div className={ styles.container }>
 
-            <div className={ styles.main }></div>
+            <div className={ styles.progress }></div>
+            <div className={ styles.buttons }></div>
             <div className={ styles.stage }></div>
             <div className={ styles.stats }></div>
         </div>
